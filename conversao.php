b50a130989c74a7ea6f6a47b0a2f4504c8ffc90c
Caio GS <caioguilherme609@gmail.com>
2026-05-17 12:07:58 -0300

    Adicionando os primeiros arquivos

---
+++ conversao.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de moedas</h1>
+        <?php 
+            // Cotação vinda da API do Banco Central
+            $início = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $início .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+            $dados = json_decode(file_get_contents($url), true);
+
+            $cotação = $dados["value"][0]["cotacaoCompra"];
+
+            // Quantos R$(reais) você tem?
+            $real = $_REQUEST["din"] ?? 0;
+
+            // Equivalência em Dólar
+            $dólar = $real / $cotação;
+
+            $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+            echo "<p>Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrão, $dólar, "USD") . "</strong></p>";
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+</body>
+</html>
